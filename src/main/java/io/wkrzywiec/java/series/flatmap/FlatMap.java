81358c02ec7aef2a65f3102d5c36f53e83e42915
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2022-10-11 19:19:21 +0200

    add spotless

--- src/main/java/io/wkrzywiec/java/series/flatmap/FlatMap.java
+++ src/main/java/io/wkrzywiec/java/series/flatmap/FlatMap.java
@@ -1,8 +1,9 @@
+/* Wojciech Krzywiec 2022 */
 package io.wkrzywiec.java.series.flatmap;
 
 public class FlatMap {
 
-    boolean isTrue() {
-        return true;
-    }
+  boolean isTrue() {
+    return true;
+  }
 }

634e092fbe72bbe89d1d1bb5b87bc8c71444abf6
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2022-10-18 07:23:46 +0200

    flatmap functions

--- src/main/java/io/wkrzywiec/java/series/flatmap/FlatMap.java
+++ src/main/java/io/wkrzywiec/java/series/flatmap/FlatMap.java
@@ -1,9 +1,23 @@
 /* Wojciech Krzywiec 2022 */
 package io.wkrzywiec.java.series.flatmap;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class FlatMap {
 
-  boolean isTrue() {
-    return true;
+  public record Author(String name, List<Book> books) {}
+
+  public record Book(String title) {}
+
+  List<String> mergeLists(List<String> left, List<String> right) {
+    return Stream.of(left, right).flatMap(List::stream).toList();
+  }
+
+  List<String> getAllBooks(List<Author> authors) {
+    return authors.stream()
+        .flatMap(author -> author.books().stream())
+        .map(book -> book.title)
+        .toList();
   }
 }
--- src/test/java/io/wkrzywiec/java/series/flatmap/FlatMapTest.java
+++ src/test/java/io/wkrzywiec/java/series/flatmap/FlatMapTest.java
@@ -1,16 +1,70 @@
 /* Wojciech Krzywiec 2022 */
 package io.wkrzywiec.java.series.flatmap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.wkrzywiec.java.series.flatmap.FlatMap.Author;
+import io.wkrzywiec.java.series.flatmap.FlatMap.Book;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class FlatMapTest {
 
-  @Test
-  void test1() {
-    var f = new FlatMap();
+  FlatMap flatMap;
 
-    assertTrue(f.isTrue());
+  @BeforeEach
+  public void setup() {
+    flatMap = new FlatMap();
+  }
+
+  @Test
+  @DisplayName("Two lists are merged")
+  void twoListsAreMerged() {
+    // given
+    var sciFiBooks =
+        List.of(
+            "Dune, by Frank Herbert",
+            "Solaris, by Stanisław Lem",
+            "A Scanner Darkly, by Philip K Dick");
+    var fantasyBooks =
+        List.of(
+            "The Lord of the Rings, by J.R.R. Tolkien",
+            "A Game of Thrones, by George R.R. Martin",
+            "Sourcery, by Terry Pratchett");
+
+    // when
+    var books = flatMap.mergeLists(sciFiBooks, fantasyBooks);
+
+    // then
+    assertEquals(
+        List.of(
+            // sci-fi books
+            "Dune, by Frank Herbert",
+            "Solaris, by Stanisław Lem",
+            "A Scanner Darkly, by Philip K Dick",
+            // fantasy books
+            "The Lord of the Rings, by J.R.R. Tolkien",
+            "A Game of Thrones, by George R.R. Martin",
+            "Sourcery, by Terry Pratchett"),
+        books);
+  }
+
+  @Test
+  @DisplayName("All books from each author are listed")
+  void allBooksAreListed() {
+    // given
+    var authors =
+        List.of(
+            new Author("Frank Herbert", List.of(new Book("Dune"))),
+            new Author(
+                "Stanisław Lem", List.of(new Book("Solaris"), new Book("Fables for Robots"))));
+
+    // when
+    var books = flatMap.getAllBooks(authors);
+
+    // then
+    assertEquals(List.of("Dune", "Solaris", "Fables for Robots"), books);
   }
 }
